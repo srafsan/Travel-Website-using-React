4a6c72625c833158ca260f4fc60a6ab22232dabb
srafsan <srafsan98@tutanota.com>
2022-01-15 22:31:04 +0600

    Home and Hero Components are done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
