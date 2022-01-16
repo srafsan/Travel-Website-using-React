6a25d74932eab5fd89127599387c8210f8192ffd
srafsan <srafsan98@tutanota.com>
2022-01-16 09:50:48 +0600

    Footer Done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../App.css'
 import Cards from '../Cards/Cards'
+import Footer from '../Footer/Footer'
 import HeroSection from '../HeroSection/HeroSection'
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <>
             <HeroSection />
             <Cards />
+            <Footer />
         </>
     )
 }
